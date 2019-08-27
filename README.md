7fbc2d3297441d21670642290db6972c697c0547
Joddev <whgksdyd112@naver.com>
2019-08-27 11:45:47 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Github label template
+
+create or edit label template
+
+### Installation
+
+```bash
+npm install
+```
+
+### How to run
+
+1. fill `token`, `repos` and `labels` in config.json
+
+2. run code
+
+3. ```bash
+   npm start
+   ```
+
---
+++ index.js
@@ -0,0 +1,30 @@
+import GitHub from 'github-api';
+import fs from 'fs';
+
+async function main() {
+  const config = JSON.parse(fs.readFileSync('config.json'));
+
+  const gh = new GitHub({
+    token: config.token
+  });
+
+  try {
+    for (const repo of config.repos) {
+      const issue = gh.getIssues(...repo.split('/'));
+      const labelSet = new Set((await issue.listLabels()).data.map(label => label.name));
+    
+      for (const label of config.labels) {
+        if (labelSet.has(label.name)) {
+          await issue.editLabel(label.name, label);
+        } else {
+          await issue.createLabel(label);
+          labelSet.add(label.name);
+        } 
+      }
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+main();
